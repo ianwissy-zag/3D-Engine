fa5d3671fd1fd773ecc963b27de2417f89574ee7
Ian Wyse <ianwissy@gmail.com>
2026-03-03 21:41:30 -0800

    updating

--- 3D-Enviroment/src/main.c
+++ 3D-Enviroment/src/main.c
@@ -3,10 +3,10 @@
 #include "player.h"
 #include <stdint.h>
 
-#define KB_DATA      0x80001600
-#define GPU_RD_ADR   0x80001500
-#define GPU_CFD_ADR  0x80001508 
-#define MTIME_ADR    0x80001020 /* SweRVolf core timer (lower 32 bits) */
+#define KB_DATA      0x80001600 // Read address for keyboard data
+#define GPU_RD_ADR   0x80001500 // Read address to determine if the VGA is done with a frame
+#define GPU_CFD_ADR  0x80001508 // Write address to indicate that program is done with a frame
+#define MTIME_ADR    0x80001020 // Read address for the rvFPGA timer module (low 32)
 
 inline int READ_REG(int dir){
     return (*(volatile unsigned *)dir);
@@ -79,18 +79,31 @@ void main() {
         last_time = current_time;
 
         // Multiply by 161 and shift right by 9 to approximate (delta_time * 65536) / 208333
+        // Avoids slow divison
         fixed32 dt_mult = (delta_time * 161) >> 9;
 
+        // This write indicates that the program is currently writing a frame
+        // and the frame buffers shouldn't be swapped
         WRITE_REG(GPU_CFD_ADR, 0);
+        // Determine the current frame that the GPU is writing for a later check
+        // to see if it is done
         int old_bram_inx = READ_REG(GPU_RD_ADR);
 
+        // Get the keyboard inputs
         readInputs();
 
+        // Move the player
         updatePlayer(dt_mult);
 
+        // determine the wall positions and write them out to the GPU
         updateRaycaster();
 
+        // Indicate to the GPU/VGA that the program is done writing and that
+        // it can switch frames
         WRITE_REG(GPU_CFD_ADR, 1);
+
+        // Wait for the GPU to finish writing its current frame and swap, so that
+        // the program can begin on the next frame. 
         while(1){
             int bram_inx = READ_REG(GPU_RD_ADR);
             if (bram_inx != old_bram_inx){
