f572ac900afe0a685a8f066b18ccbfe12fbe9ff5
Ian Wyse <ianwissy@gmail.com>
2026-03-17 15:26:56 -0700

    Reduced pipelining in GPU, may fail

--- Integration/src/main.c
+++ Integration/src/main.c
@@ -38,8 +38,9 @@ const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
     {R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R}
 };
 
-// These are only the cubes that are active, as subeset of total cubes created on game start
-CubeEntity* visibleList[MAX_ACTIVE_CUBES];
+// These are only the cubes that are active, a subset of total cubes created on game start
+CubeEntity* visibleList[MAX_ACTIVE_CUBES];4
+// These are all the created cubes.
 extern CubeEntity world_cubes[MAX_ACTIVE_CUBES];
 
 void readInputs(){
